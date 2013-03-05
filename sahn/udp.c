caa0b35d911fe0baab88f31bf584dd354a35a172
Justin Brewer <jzb0012@auburn.edu>
2013-03-05 11:29:22 -0600

    Implement udp_recv
    I doubt source determination will ever be solved cleanly

--- sahn/udp.c
+++ sahn/udp.c
@@ -48,5 +48,6 @@ int udp_send(uint16_t destination, uint8_t* data, uint32_t data_size){
 }
 
 int udp_recv(uint16_t* source, uint8_t* buffer, uint32_t buffer_size){
-
+  //TODO determine source simulated address
+  return recvfrom(udp_socket,buffer,buffer_size,0,NULL,NULL);
 }
