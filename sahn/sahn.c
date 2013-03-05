019714ea2b30676d4d2cb8f42543f8be19ea3648
Justin Brewer <jzb0012@auburn.edu>
2013-03-04 21:37:37 -0600

    Whoops
    C doesn't have pass-by-reference. What was I thinking?

--- sahn/sahn.c
+++ sahn/sahn.c
@@ -22,6 +22,6 @@ int sahn_send(uint16_t destination, uint8_t* data, uint32_t data_size) {
 
 }
 
-int sahn_recv(uint16_t& source, uint8_t* buffer, uint32_t buffer_size) {
+int sahn_recv(uint16_t* source, uint8_t* buffer, uint32_t buffer_size) {
 
 }
--- sahn/sahn.h
+++ sahn/sahn.h
@@ -12,4 +12,4 @@ int sahn_watch_file(const char* file);
 int sahn_cleanup();
 
 int sahn_send(uint16_t destination, uint8_t* data, uint32_t data_size);
-int sahn_recv(uint16_t& source, uint8_t* buffer, uint32_t buffer_size);
+int sahn_recv(uint16_t* source, uint8_t* buffer, uint32_t buffer_size);
