69aa1353894fe74ffef300efd16043c2f37d4b33
Justin Brewer <jzb0012@auburn.edu>
2013-03-04 21:20:54 -0600

    Give public-facing interface proper naming scheme

---
+++ sahn/sahn.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+#define SAHN_MAX_DATA_SIZE 116
+
+int sahn_init();
+
+int sahn_load_file(const char* file);
+int sahn_watch_file(const char* file);
+
+int sahn_cleanup();
+
+int sahn_send(uint16_t destination, uint8_t* data, uint32_t data_size);
+int sahn_recv(uint16_t& source, uint8_t* buffer, uint32_t buffer_size);
