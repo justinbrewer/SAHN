985ccdb99ebab6a9e61352578ee9099501657b0e
Justin Brewer <jzb0012@auburn.edu>
2013-04-10 06:37:46 -0500

    Basic interface documentation

--- sahn/sahn.c
+++ sahn/sahn.c
@@ -25,6 +25,10 @@
 
 #define SAHN_EXPORT __attribute__((visibility("default")))
 
+/** \brief Initialize a config struct
+ *
+ *  Allocates a new config struct and populates it with default values.
+ */
 SAHN_EXPORT
 struct sahn_config_t* sahn_config_create(){
   struct sahn_config_t* config = (struct sahn_config_t*)malloc(sizeof(struct sahn_config_t));
@@ -36,11 +40,21 @@ struct sahn_config_t* sahn_config_create(){
   return config;
 }
 
+/** \brief Free config struct
+ */
 SAHN_EXPORT
 int sahn_config_destroy(struct sahn_config_t* config){
   free(config);
 }
 
+/** \brief Initialise simulator
+ *
+ *  Initialises simulator modules and starts network routing thread.
+ *
+ *  \param topology_file Text file describing the simulated network layout
+ *  \param node_address Address of the node that this instance should represent
+ *  \parma config Configuration options. If NULL, defaults are assumed.
+ */
 SAHN_EXPORT
 int sahn_init(const char* topology_file, uint16_t node_address, struct sahn_config_t* config) {
   bool free_config = false;
@@ -60,6 +74,10 @@ int sahn_init(const char* topology_file, uint16_t node_address, struct sahn_conf
   return 0; //TODO: Proper error checking
 }
 
+/** \brief Shutdown simulator
+ *
+ *  Stops simulator and cleans up modules
+ */
 SAHN_EXPORT
 int sahn_cleanup() {
   net_cleanup();
@@ -69,11 +87,27 @@ int sahn_cleanup() {
   return 0;
 }
 
+/** \brief Send a packet
+ *
+ *  Sends a packet over the simulated network
+ *
+ *  \param destination Address of destination node
+ *  \param data Packet to send
+ *  \param data_size Size of packet in bytes
+ */
 SAHN_EXPORT
 int sahn_send(uint16_t destination, void* data, uint32_t data_size) {
   return net_send(destination,data,data_size);
 }
 
+/** \brief Receive a packet
+ *
+ *  Blocks until a packet arrives on the simulated network.
+ *
+ *  \param source Set to the address the packet originated from
+ *  \param buffer Buffer to store incoming packet in
+ *  \param buffer_size Size of buffer in bytes
+ */
 SAHN_EXPORT
 int sahn_recv(uint16_t* source, void* buffer, uint32_t buffer_size) {
   return net_recv(source,buffer,buffer_size);
