6b38b809d7cd8f8248b38f5e39f0889570e4727a
Justin Brewer <jzb0012@auburn.edu>
2013-04-22 18:49:31 -0500

    Skeleton OLSR implementation

---
+++ src/route/olsr.c
@@ -0,0 +1,34 @@
+/* This file is part of libsahn
+ * Copyright (c) 2013 Justin Brewer
+ *
+ * libsahn is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "route.h"
+
+int route_init(struct sahn_config_t* config){
+  return 0;
+}
+
+int route_cleanup(){
+  return 0;
+}
+
+int route_update_links(){
+  return 0;
+}
+
+int route_dispatch_packet(struct net_packet_t* packet){
+  return 0;
+}
